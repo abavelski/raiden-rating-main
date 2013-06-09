4e585b3d8a6bcadcca410461e0e9abe051137e75
aba <bavelski@gmail.com>
2013-06-09 11:31:11 +0200

    added test, removed ace

--- src/test/java/mk/arena/raiden/main/plugin/rating/initial/InitialTest.java
+++ src/test/java/mk/arena/raiden/main/plugin/rating/initial/InitialTest.java
@@ -2,15 +2,10 @@ package mk.arena.raiden.main.plugin.rating.initial;
 
 import mk.arena.raiden.api.bean.rating.RatingRequest;
 import mk.arena.raiden.main.plugin.rating.RatingPlugin;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.annotation.Resource;
-import javax.inject.Inject;
 import java.math.BigDecimal;
-
 import static junit.framework.TestCase.assertEquals;
 
 
@@ -19,18 +14,28 @@ import static junit.framework.TestCase.assertEquals;
  * Date: 08/06/13
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+
 public class InitialTest {
 
-    @Inject
-    RatingPlugin initial;
+    private RatingPlugin initial;
+
+    @Before
+    public void setup() {
+        initial = new Initial();
+    }
+
 
     @Test
     public void testZeroAmount() {
         RatingRequest request = new RatingRequest();
         request.setAmount(0);
-        assertEquals(initial.calculatePrice(request, BigDecimal.ZERO), BigDecimal.ZERO);
+        assertEquals(initial.calculatePrice(request, BigDecimal.valueOf(0.29)), BigDecimal.ZERO);
     }
 
+    @Test
+    public void testNonZeroAmount() throws Exception {
+        RatingRequest request = new RatingRequest();
+        request.setAmount(245);
+        assertEquals(initial.calculatePrice(request, BigDecimal.valueOf(0.29)), BigDecimal.valueOf(0.29));
+    }
 }
