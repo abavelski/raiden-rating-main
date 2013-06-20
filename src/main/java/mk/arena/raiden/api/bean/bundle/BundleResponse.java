a6f675176e562cac6ebd0d9839f578d8cd517613
aba <bavelski@gmail.com>
2013-06-20 15:43:29 +0200

    added campaign codes

--- src/main/java/mk/arena/raiden/api/bean/bundle/BundleResponse.java
+++ src/main/java/mk/arena/raiden/api/bean/bundle/BundleResponse.java
@@ -1,8 +1,31 @@
 package mk.arena.raiden.api.bean.bundle;
 
+import mk.arena.raiden.api.bean.ChargeLine;
+
+import java.util.List;
+
 /**
  * User: aba
  * Date: 6/11/13
  */
 public class BundleResponse {
+
+    private List<ChargeLine> chargeLines;
+    private long restAmount;
+
+    public List<ChargeLine> getChargeLines() {
+        return chargeLines;
+    }
+
+    public void setChargeLines(List<ChargeLine> chargeLines) {
+        this.chargeLines = chargeLines;
+    }
+
+    public long getRestAmount() {
+        return restAmount;
+    }
+
+    public void setRestAmount(long restAmount) {
+        this.restAmount = restAmount;
+    }
 }
